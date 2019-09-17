df2dcc529016af9827e3818764de3461836dc356
Ross Hill <rosshill@protonmail.com>
2019-09-17 12:14:48 -0400

    Fix alignment issue

--- client/src/components/findDistrict.js
+++ client/src/components/findDistrict.js
@@ -174,6 +174,8 @@ const FindDistrict = inject(`store`)(
                 color: #950451;
                 display: flex;
                 align-items: center;
+                justify-content: center;
+                align-items: center;
               `}
             >
               <IoMdPin />
