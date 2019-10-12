24b6a4eab068f371f048313d7286ce16dcd7a515
Ross Hill <rosshill@protonmail.com>
2019-10-12 14:51:15 -0400

    Make sharing work on mobile Move some dependencies to devDeps

--- client/src/components/shareButtons.js
+++ client/src/components/shareButtons.js
@@ -7,17 +7,17 @@ import {
   TwitterShareButton,
   RedditShareButton,
 } from "react-share";
-
 import {
   FaRedditAlien,
   FaFacebookF,
   FaLinkedinIn,
   FaTwitter,
+  FaShareSquare,
 } from "react-icons/fa";
-
 import { useStaticQuery, graphql } from "gatsby";
+import webShare from "react-web-share-api";
 
-function ShareLinks({ page, title }) {
+function ShareLinks({ page, title, share, isSupported: shareApiSupported }) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -32,79 +32,113 @@ function ShareLinks({ page, title }) {
     `
   );
   const fullTitle = `${title} | ${site.siteMetadata.title}`;
-  return (
-    <ClassNames>
-      {({ css: style }) => (
-        <div
-          css={css`
-            padding-top: 1rem;
-            display: flex;
-            align-items: center;
-            .SocialMediaShareButton {
-              cursor: pointer;
-              :hover {
-                opacity: 0.9;
-              }
-              margin: 0 0.3rem;
-              color: white;
-              display: inline-block;
-              height: 1.6rem;
-              width: 1.6rem;
-              font-size: 0.9rem;
-              display: flex;
-              justify-content: center;
-              align-items: center;
-              border-radius: 50%;
-              overflow: hidden;
-            }
-          `}
-        >
-          <span
+  if (!shareApiSupported) {
+    return (
+      <ClassNames>
+        {({ css: style }) => (
+          <div
             css={css`
-              padding-right: 0.4rem;
+              padding-top: 1.5rem;
+              display: flex;
+              align-items: center;
+              .SocialMediaShareButton {
+                cursor: pointer;
+                :hover {
+                  opacity: 0.9;
+                }
+                margin: 0 0.3rem;
+                color: white;
+                display: inline-block;
+                height: 1.6rem;
+                width: 1.6rem;
+                font-size: 0.9rem;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                border-radius: 50%;
+                overflow: hidden;
+              }
             `}
           >
-            Share:
-          </span>
-          <FacebookShareButton
-            className={style`
+            <span
+              css={css`
+                padding-right: 0.4rem;
+              `}
+            >
+              Share:
+            </span>
+            <FacebookShareButton
+              className={style`
               background-color: #3B579D;
             `}
-            url={`${site.siteMetadata.url}${page}`}
-            quote={fullTitle}
-          >
-            <FaFacebookF />
-          </FacebookShareButton>
-          <TwitterShareButton
-            className={style`
+              url={`${site.siteMetadata.url}${page}`}
+              quote={fullTitle}
+            >
+              <FaFacebookF />
+            </FacebookShareButton>
+            <TwitterShareButton
+              className={style`
               background-color: #2CAAE1;
             `}
-            url={`${site.siteMetadata.url}${page}`}
-            title={`${site.siteMetadata.description}. Take a look!`}
-          >
-            <FaTwitter />
-          </TwitterShareButton>
-          <RedditShareButton
-            className={style`
+              url={`${site.siteMetadata.url}${page}`}
+              title={`${site.siteMetadata.description}. Take a look!`}
+            >
+              <FaTwitter />
+            </TwitterShareButton>
+            <RedditShareButton
+              className={style`
               background-color: #FF4500 !important;
             `}
-            title={fullTitle}
-            url={`${site.siteMetadata.url}${page}`}
-          >
-            <FaRedditAlien />
-          </RedditShareButton>
-          <LinkedinShareButton
-            className={style`
+              title={fullTitle}
+              url={`${site.siteMetadata.url}${page}`}
+            >
+              <FaRedditAlien />
+            </RedditShareButton>
+            <LinkedinShareButton
+              className={style`
               background-color: #007BB6;
             `}
-            url={`${site.siteMetadata.url}${page}`}
-          >
-            <FaLinkedinIn />
-          </LinkedinShareButton>
-        </div>
-      )}
-    </ClassNames>
-  );
+              url={`${site.siteMetadata.url}${page}`}
+            >
+              <FaLinkedinIn />
+            </LinkedinShareButton>
+          </div>
+        )}
+      </ClassNames>
+    );
+  } else {
+    return (
+      <button
+        css={css`
+          margin-top: 1.5rem;
+          font-size: 0.9rem;
+          line-height: 2rem;
+          padding: 0 1rem;
+          border-radius: 4px;
+          border: none;
+          color: white;
+          background-color: #950451;
+          font-weight: bold;
+          box-sizing: content-box;
+          cursor: pointer;
+          display: inline-flex;
+          justify-content: center;
+          align-items: center;
+          :hover {
+            opacity: 0.9;
+          }
+          svg {
+            margin-right: 0.4rem;
+            font-size: 0.85rem;
+          }
+        `}
+        onClick={share}
+      >
+        <FaShareSquare />
+        Share
+      </button>
+    );
+  }
 }
 
 ShareLinks.propTypes = {
@@ -112,4 +146,4 @@ ShareLinks.propTypes = {
   title: PropTypes.string,
 };
 
-export default ShareLinks;
+export default webShare()(ShareLinks);
