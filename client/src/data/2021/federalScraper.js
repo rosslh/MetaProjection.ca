05dd2a3ac8ba8c10320366f1ba70f3d550e8dc0d
Ross Hill <ross@rosshill.ca>
2021-08-15 11:16:11 -0400

    Set user agent

--- client/src/data/2021/federalScraper.js
+++ client/src/data/2021/federalScraper.js
@@ -136,7 +136,7 @@ async function getCBC() {
     ignoreHTTPSErrors: true,
   }); // using puppeteer because they load dynamic content
   const page = await browser.newPage();
-
+  await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
   await page.goto(url);
 
   await page.addScriptTag({ content: `${getShortPartyString}` });
